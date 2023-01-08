3ebe4ec689a73dbc14b805191126ca04556cbe53
Jyrki Heinonen <jyrki.heinonen@iki.fi>
2023-01-08 08:38:43 +0000

    First version

---
+++ api/getHealth/index.ts
@@ -0,0 +1,18 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    const responseMessage = {
+        time: Date.UTC.toString(),
+        status: 'OK',
+        text: 'Tämä on OK'
+    }
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+
+};
+
+export default httpTrigger;
---
+++ app/src/App.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [data, setData] = useState('test')
+
+useEffect(() => {
+  (async function() {
+    const { status } = await (await fetch('api/getHealth')).json()
+    setData(status)
+  })();
+})
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Status: {data}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
